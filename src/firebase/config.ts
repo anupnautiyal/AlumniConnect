f34865bb7dd03d91cf2c685e627a7a7238ff8489
Anup Nautiyal <anupn8083@gmail.com>
2026-02-03 13:02:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3739195243-2b65e",
+  "appId": "1:604690911890:web:228115a9b097b3e7ab8c4b",
+  "apiKey": "AIzaSyCTPU3r--39edWKbUn9f1ppdvhL574iAH4",
+  "authDomain": "studio-3739195243-2b65e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "604690911890"
+};
