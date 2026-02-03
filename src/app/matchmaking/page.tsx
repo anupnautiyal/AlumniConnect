60c390fdbeac3a83ee6a6988b8fe27b0761bb3d7
Anup Nautiyal <anupn8083@gmail.com>
2026-02-03 13:24:23 +0000

    fix error of directory route

--- src/app/matchmaking/page.tsx
+++ src/app/matchmaking/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation"
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function MatchmakingPage() {
-  redirect("/")
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.push('/');
+  }, [router]);
+
+  return null;
 }
